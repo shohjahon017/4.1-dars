c43112e2cf5e506b65c0d35b416060d5663d114c
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-05-31 14:37:03 +0500

    main

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// 1-masala
+// let a = +prompt("Kvadratning tomonini kiriting");
+// let P = 4 * a;
+// console.log(P);
+
+//2-masala
+//let a = +prompt("Kvadratning yuzasini aniqlang");
+//let S = a * a;
+//console.log(S);
+
+//3-masala
+
+//4-masala
+// let d = +prompt("Aylananig diametrini aniqlang");
+//let L = Math.PI * d;
+//console.log(L);
+
+//5-masala
+//let a = +prompt("Kubning hajmi va to`la sirtini toping");
+//let V = a * a * a;
+//let S = 6 * a * a;
+//console.log(V + "" + S);
+
+//6-masala
+// let a = +prompt("Paralelopipedning hajmini va to`la sirtini toping");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+// console.log(S);
+// console.log(V);
+
+//7-masala
