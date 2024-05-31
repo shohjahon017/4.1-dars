666c44c648bdb83525dd5803c797dcccdb14b91a
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-05-31 16:05:30 +0500

    17-masalani buldim

--- js/index.js
+++ js/index.js
@@ -9,17 +9,25 @@
 //console.log(S);
 
 //3-masala
+// let a = +prompt("To`g`ri to`rtburchakning yuzasi va perimetrini toping");
+// let b = +prompt("b sonni kiriting");
+// let S = a * b;
+// let P = 2 * (a + b);
+// console.log(S);
+// console.log(P);
 
 //4-masala
 // let d = +prompt("Aylananig diametrini aniqlang");
-//let L = Math.PI * d;
-//console.log(L);
+// const MathPI = 3.14;
+// let L = Math.PI * d;
+// console.log(L);
 
 //5-masala
 //let a = +prompt("Kubning hajmi va to`la sirtini toping");
 //let V = a * a * a;
 //let S = 6 * a * a;
-//console.log(V + "" + S);
+//console.log(V  );
+//console.log(S);
 
 //6-masala
 // let a = +prompt("Paralelopipedning hajmini va to`la sirtini toping");
@@ -31,3 +39,75 @@
 // console.log(V);
 
 //7-masala
+// let R = +prompt("Doirani uzunligi va yuzasini toping");
+// const MathPI = 3.14;
+// let L = 2 * MathPI * R;
+// let S = MathPI * R * R;
+// console.log(L);
+// console.log(S);
+
+//8-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let i = a + b;
+// console.log(i);
+// alert(" o`rta arifmetikasi: " + i);
+
+//9-masala
+// let R1 = +prompt(" r1 sonni kiriting");
+// let R2 = +prompt("r2 sonni kiriting");
+// const MathPI = 3.14;
+// let S1 = MathPI * R1;
+// let S2 = MathPI * R2;
+// let S3 = MathPI * (R1 - R2);
+// console.log(S1);
+// console.log(S2);
+// console.log(S3);
+
+// 10-masala
+// let Tf = +prompt("Temperatura qiymatini Tc Farengeytga o`tkazin");
+// let Tc = ((Tf - 32) * 5) / 9;
+// console.log(Tc);
+
+// 11-masala
+// let L = +prompt("L ni qiymatini kiriting");
+// let m = L / 100;
+// console.log(m);
+
+//12-masala
+// let M = +prompt("M ni qiymatini kiriting");
+// let t = M / 1000;
+// console.log(''+a+'tonnaga teng');
+
+// 13-masala
+// let bayt = +prompt("baytni kiriting");
+// let kb = bayt / 1024;
+// console.log(kb);
+
+//14-masala
+// let A = +prompt(" A kesmani qiymatini bering");
+// let B = +prompt("B kesmani qiymatini kiriting ");
+// let C = A / B;
+// console.log(A);
+
+//15-masala
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let c = a % b;
+// console.log(c);
+
+//16-masala
+// let a = +prompt(" ikki xonali sonni kiriting");
+// let b = a % 10;
+// let c = a / 10;
+// console.log(c);
+// console.log(b);
+
+//17-masala
+// let a = +prompt("Ikki xonali sonni kiriting ");
+// let b = a % 10;
+// let c = a / 10;
+// let d = (a % 10) + a / 10;
+// console.log(d);
+
+//18-masala
